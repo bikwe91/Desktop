796ea308dfc2fa68c0ee3a0ffb080d56308e6611
Fabrice Bikwe <fabricebikwe91@gmail.com>
2026-05-22 16:31:10 -0400

    Initial Commit

---
+++ Bikwe_Implement_Basic_Python/main.py
@@ -0,0 +1,27 @@
+# Exercise 2
+# Ask user for name and print welcome message
+name = input("Enter your name: ")
+print("Hello", name)
+
+# Exercise 3
+# Ask for hours and rate and calculate gross pay
+hours = input("Enter Hours: ")
+rate = input("Enter Rate: ")
+pay = float(hours) * float(rate)
+print("Pay:", pay)
+
+# Exercise 4
+# Calculate and display expression results
+width = 17
+height = 12.0
+
+print(width // 2)
+print(width / 2.0)
+print(height / 3)
+print(1 + 2 * 5)
+
+# Exercise 5
+# Convert Celsius to Fahrenheit
+celsius = input("Enter Celsius temperature: ")
+fahrenheit = (float(celsius) * 9 / 5) + 32
+print("Fahrenheit:", fahrenheit)
